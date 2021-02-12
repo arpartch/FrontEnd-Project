a5bb5201cba5f855545c7163e5b087a144454e91
denisha garner <dgarner101@icloud.com>
2021-02-11 19:49:16 -0500

    update social media icons landing page

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,4 @@
-var myInput = document.getElementById("password");
+var myInput = document.getElementById("psw");
 var letter = document.getElementById("letter");
 var capital = document.getElementById("capital");
 var number = document.getElementById("number");
@@ -18,7 +18,7 @@ myInput.onblur = function() {
 myInput.onkeyup = function() {
   // Validate lowercase letters
   var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) { 
+  if(myInput.value.match(lowerCaseLetters)) {
     letter.classList.remove("invalid");
     letter.classList.add("valid");
   } else {
@@ -26,10 +26,9 @@ myInput.onkeyup = function() {
     letter.classList.add("invalid");
 }
 
-
   // Validate capital letters
   var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) { 
+  if(myInput.value.match(upperCaseLetters)) {
     capital.classList.remove("invalid");
     capital.classList.add("valid");
   } else {
@@ -39,7 +38,7 @@ myInput.onkeyup = function() {
 
   // Validate numbers
   var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) { 
+  if(myInput.value.match(numbers)) {
     number.classList.remove("invalid");
     number.classList.add("valid");
   } else {
